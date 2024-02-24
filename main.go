b165d22b301ad778261df08f49f674b25d08e8ac
mercy34mercy <etukobamasatyan@gmail.com>
2024-02-24 13:13:40 +0900

    logを変更

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
+
 	"net/http"
 
 	_ "github.com/lib/pq"
 	"github.com/mercy34mercy/go-http-server/config"
 	"github.com/mercy34mercy/go-http-server/handler/health"
 	v1 "github.com/mercy34mercy/go-http-server/handler/v1"
+	"github.com/mercy34mercy/go-http-server/log"
 	"github.com/mercy34mercy/go-http-server/repository/user/psql"
 	"github.com/mercy34mercy/go-http-server/usecase"
 )
@@ -18,7 +19,7 @@ func main() {
 	// psqlのcliを生成
 	client, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", config.POSTGRESQL_HOST(), config.POSTGRESQL_PORT(), config.POSTGRESQL_USER_NAME(), config.POSTGRESQL_PASSWORD(), config.POSTGRESQL_DB_NAME()))
 	if err != nil {
-		log.Fatal("failed to connect to database %w", err)
+		log.Fatal(err, fmt.Errorf("failed to connect to database: %w", err))
 	}
 	defer client.Close()
 
