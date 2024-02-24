d2d3c2e2f1fe6dbb510d7e95b901fd3785869619
mercy34mercy <etukobamasatyan@gmail.com>
2024-02-24 13:00:48 +0900

    logを出す

--- config/config.go
+++ config/config.go
@@ -17,6 +17,7 @@ func init() {
 func POSTGRESQL_USER_NAME() string {
 	v, ok := os.LookupEnv("POSTGRESQL_USER_NAME")
 	if !ok {
+		log.Error(nil, "POSTGRES_USER_NAME is not set")
 		panic("POSTGRES_USER_NAME is not set")
 	}
 	return v
@@ -25,6 +26,7 @@ func POSTGRESQL_USER_NAME() string {
 func POSTGRESQL_PASSWORD() string {
 	v, ok := os.LookupEnv("POSTGRESQL_PASSWORD")
 	if !ok {
+		log.Error(nil, "POSTGRES_PASSWORD is not set")
 		panic("POSTGRES_PASSWORD is not set")
 	}
 	return v
@@ -33,6 +35,7 @@ func POSTGRESQL_PASSWORD() string {
 func POSTGRESQL_DB_NAME() string {
 	v, ok := os.LookupEnv("POSTGRESQL_DB_NAME")
 	if !ok {
+		log.Error(nil, "POSTGRES_DB_NAME is not set")
 		panic("POSTGRES_DB_NAME is not set")
 	}
 	return v
@@ -41,6 +44,7 @@ func POSTGRESQL_DB_NAME() string {
 func POSTGRESQL_HOST() string {
 	v, ok := os.LookupEnv("POSTGRESQL_HOST")
 	if !ok {
+		log.Error(nil, "POSTGRES_HOST is not set")
 		panic("POSTGRES_HOST is not set")
 	}
 	return v
@@ -49,6 +53,7 @@ func POSTGRESQL_HOST() string {
 func POSTGRESQL_PORT() string {
 	v, ok := os.LookupEnv("POSTGRESQL_PORT")
 	if !ok {
+		log.Error(nil, "POSTGRES_PORT is not set")
 		panic("POSTGRES_PORT is not set")
 	}
 	return v
@@ -57,6 +62,7 @@ func POSTGRESQL_PORT() string {
 func POSTGRESQL_MAX_POOL_CONNS() string {
 	v, ok := os.LookupEnv("POSTGRESQL_MAX_POOL_CONNS")
 	if !ok {
+		log.Error(nil, "POSTGRES_MAX_POOL_CONNS is not set")
 		panic("POSTGRES_MAX_POOL_CONNS is not set")
 	}
 	return v
